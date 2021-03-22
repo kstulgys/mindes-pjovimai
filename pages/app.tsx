8b2dbae462543f06d20bc4630a05c2732cc9a4bf
kstulgys <karolis.stulgys@gmail.com>
2021-03-22 11:53:26 +0200

    wip

--- pages/app.tsx
+++ pages/app.tsx
@@ -28,6 +28,184 @@ import { useStore } from '../store'
 import { DragHandleIcon, CloseIcon } from '@chakra-ui/icons'
 // import { IconName } from "react-icons/fi";
 import XLSX from 'xlsx'
+import {
+  Page,
+  Text as TextPDF,
+  View,
+  Document,
+  StyleSheet,
+  PDFDownloadLink,
+  PDFViewer,
+  Font,
+} from '@react-pdf/renderer'
+
+// Font.register({
+//   family: 'Montserrat',
+//   src: 'https://fonts.gstatic.com/s/montserrat/v15/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2',
+// })
+
+const styles = StyleSheet.create({
+  body: {
+    paddingTop: 35,
+    paddingBottom: 65,
+    paddingHorizontal: 35,
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    fontSize: '2rem',
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontFamily: 'Oswald',
+  },
+  author: {
+    fontSize: 12,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  subtitle: {
+    fontSize: 18,
+    margin: 12,
+    fontFamily: 'Oswald',
+  },
+  text: {
+    margin: 12,
+    fontSize: 14,
+    textAlign: 'justify',
+    fontFamily: 'Montserrat',
+  },
+  image: {
+    marginVertical: 15,
+    marginHorizontal: 100,
+  },
+  header: {
+    fontSize: 12,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'grey',
+  },
+  pageNumber: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'grey',
+  },
+})
+
+function TableHead() {
+  const result1D = useStore((store) => store.result1D)
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        borderStyle: 'solid',
+        fontSize: 12,
+        border: '1px solid',
+        borderWidth: 1,
+        borderColor: 'black',
+      }}
+    >
+      <TextPDF style={{ width: '20%' }}>Quantity</TextPDF>
+      <TextPDF style={{ width: '20%' }}>Stock length</TextPDF>
+      <TextPDF style={{ width: '40%' }}>Cut list</TextPDF>
+      <TextPDF style={{ width: '20%' }}>Waste (mm)</TextPDF>
+    </View>
+  )
+}
+function TableRow({ count, stockSize, items, capacity }) {
+  const result1D = useStore((store) => store.result1D)
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        borderStyle: 'solid',
+        fontSize: 12,
+        border: '1px solid',
+        borderWidth: 1,
+        borderColor: 'black',
+      }}
+    >
+      <TextPDF style={{ width: '20%' }}>{count}</TextPDF>
+      <TextPDF style={{ width: '20%' }}>{stockSize}</TextPDF>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap', width: '40%' }}>
+        {items.map((item, index) => (
+          <TextPDF key={index}> [{item}] </TextPDF>
+        ))}
+      </View>
+      <TextPDF style={{ width: '20%' }}>{capacity}</TextPDF>
+    </View>
+  )
+}
+
+function MyDocument() {
+  const result1D = useStore((store) => store.result1D)
+  // if (!result1D.length) return null
+  return (
+    <Document>
+      <Page size="A4" style={styles.body}>
+        <TableHead />
+        {result1D.map(([key, item]) => {
+          return <TableRow key={key} {...item} />
+        })}
+
+        {/* <pre>{JSON.stringify(result1D, null, 2)}</pre> */}
+        {/* <Table size="sm"> */}
+        {/* <Thead>
+              <Tr>
+                <Th>Quantity</Th>
+                <Th>Stock length</Th>
+                <Th>Cut list</Th>
+                <Th>Waste (mm)</Th>
+              </Tr>
+            </Thead> */}
+        {/* <Tbody>
+              {result1D.map(([key, { count, capacity, items, stockSize, capacityPercent }]) => {
+                return (
+                  <Tr key={key}>
+                    <Td>{count}</Td>
+                    <Td>{stockSize}</Td>
+                    <Td px="1" display="flex" flexWrap="wrap">
+                      {items.map((item, idx) => {
+                        return (
+                          <Box
+                            key={idx}
+                            rounded="sm"
+                            border="1px solid"
+                            m="1"
+                            px="1"
+                            borderColor="gray.600"
+                            color="gray.600"
+                          >
+                            {item}
+                          </Box>
+                        )
+                      })}
+                    </Td>
+                    <Td>{capacity}</Td>
+                  </Tr>
+                )
+              })}
+            </Tbody> */}
+        {/* </Table> */}
+        <TextPDF
+          style={styles.pageNumber}
+          render={({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`}
+          fixed
+        />
+      </Page>
+    </Document>
+  )
+}
 
 function AppPage() {
   const { isLoading, user } = useAuthUser()
@@ -42,6 +220,12 @@ function AppPage() {
     <Layout>
       <Box as="main" mx="auto" width="full" py={['12']}>
         <ButtonsSwitch1D2D />
+        {/* <PDFDownloadLink document={<MyDocument />} fileName="hello world">
+          <Text>Download PDF</Text>
+        </PDFDownloadLink> */}
+        {/* <PDFViewer style={{ width: '100%', height: '100%' }}>
+          <MyDocument />
+        </PDFViewer> */}
         {is1DView ? (
           <Stack direction={['column', 'row']} spacing="6" width="full">
             <Stack width={['100%', '40%']} bg="white" p="6" rounded="md" boxShadow="base">
@@ -285,7 +469,7 @@ function ResultView() {
   return (
     <Stack isInline fontSize="xs" bg="white" p="6" rounded="md" boxShadow="base" overflowX="auto">
       <Stack width="full">
-        <Table size="sm" id="export-to-xls-table">
+        <Table size="sm">
           <Thead>
             <Tr>
               <Th>Quantity</Th>
@@ -372,9 +556,14 @@ function ButtonsResultExport() {
         </Button>
       </Box>
       <Box>
-        <Button width="32" bg="gray.900" color="white" boxShadow="base" _hover={{}}>
-          Export PDF
-        </Button>
+        <PDFDownloadLink
+          document={<MyDocument />}
+          fileName={'stock_cut_result_' + Date.now().toString()}
+        >
+          <Button width="32" bg="gray.900" color="white" boxShadow="base" _hover={{}}>
+            Export PDF
+          </Button>
+        </PDFDownloadLink>
       </Box>
     </Stack>
   )
