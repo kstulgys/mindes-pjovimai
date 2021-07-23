f203b7760d3698affab18fa0460f13401513fa60
kstulgys <karolis.stulgys@gmail.com>
2021-07-24 00:06:36 +0300

    Wip

--- pages/api/calculate.ts
+++ pages/api/calculate.ts
@@ -1,8 +1,12 @@
 import { loopCalculation } from "../../utils";
 
 export default (req, res) => {
-  const { stockItems, cutItems, bladeSize, constantD } = req.body;
-  console.log({ stockItems, cutItems, bladeSize, constantD });
-  const result = loopCalculation(stockItems, cutItems, bladeSize, constantD);
-  res.status(200).json(result);
+  try {
+    const { stockItems, cutItems, bladeSize, constantD } = req.body;
+    console.log({ stockItems, cutItems, bladeSize, constantD });
+    const result = loopCalculation(stockItems, cutItems, bladeSize, constantD);
+    res.status(200).json(result);
+  } catch {
+    res.status(200).json({ error: "Server responded with error" });
+  }
 };
