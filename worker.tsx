90afad446f502f48a9c1874b1f4c3ca6b7e8bcda
SetMasiulisAsObject <mindaug.masiulis@gmail.com>
2021-10-17 11:42:59 +0300

    loopcalculation skipped

--- worker.tsx
+++ worker.tsx
@@ -5,7 +5,8 @@ onmessage = function (event){
     const { stockItems, cutItems, bladeSize, constantD } = event.data;
     console.log({ stockItems, cutItems, bladeSize, constantD });
     const result = loopCalculation(stockItems, cutItems, bladeSize, constantD);
-    postMessage(result);
+    //postMessage(result);
+    postMessage ({ error: 'Apeinama skaiciavimu funkcija' })
   } catch (error) {
     postMessage({ error: 'error' });
   }
