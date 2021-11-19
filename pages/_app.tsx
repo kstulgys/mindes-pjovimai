6aecfb51e47d4f6d6095eaf54e57ec935772ef88
kstulgys <karolis.stulgys@gmail.com>
2021-11-19 07:16:49 +0000

    commit from gitpod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,9 +19,6 @@ function MyApp({ Component, pageProps }) {
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Cutting stock optimization App. Helps to solve 1D Cutting stock problem." />
-        {/* <link rel="stylesheet" href="https://jsuites.net/v4/jsuites.css" type="text/css" />
-        <link rel="stylesheet" href="https://bossanova.uk/Jspreadsheet/v4/jexcel.css" type="text/css" /> */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
